37f4555e128876db67aa44429635dd7240bbd405
Qiumin Yin <mqyin@g.ucla.edu>
2017-11-21 19:28:33 -0800

    start session form

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
-// import { Provider } from 'react-redux';
-// import { HashRouter } from 'react-router-dom';
-//import App from './App';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
 
-// const Root = ({ store }) => (
-//   <Provider store={ store }>
-//     <HashRouter>
-//       <h1>React is working</h1>
-//     </HashRouter>
-//   </Provider>
-// );
-
-const Root = () => (
-  <h1>React is working</h1>
+const Root = ({ store }) => (
+  <Provider store={ store }>
+    <HashRouter>
+      <App/>
+    </HashRouter>
+  </Provider>
 );
-
 
 export default Root;
---
+++ frontend/components/session_form/session_form.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+class SessionForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.loggedIn) {
+      this.props.history.push('/conversations');
+    }
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const user = this.state;
+    this.props.processForm({user});
+  }
+
+  navLink() {
+    if (this.props.formType === 'login') {
+      return <p>New user? <Link to="/signup">Sign up</Link> instead</p>;
+    } else {
+      return <p>Returning user? <Link to="/login">Log in</Link> instead</p>;
+    }
+  }
+
+  renderErrors() {
+    return(
+      <ul>
+        {this.props.errors.map((error, i) => (
+          <li key={`error-${i}`}>
+            {error}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+
+    return (
+      <div className="login-form-container">
+        <form onSubmit={this.handleSubmit} className="login-form-box">
+          Message-Me!
+          <br/>
+          <div className="login-form">
+            <br/>
+            <label>Username:
+              <input type="text"
+                value={this.state.username}
+                onChange={this.update('username')}
+                className="login-input"
+              />
+            </label>
+            <br/>
+            <label>Password:
+              <input type="password"
+                value={this.state.password}
+                onChange={this.update('password')}
+                className="login-input"
+              />
+            </label>
+            <br/>
+            <input type="submit" value="Submit" />
+          </div>
+        </form>
+        {this.navLink()}
+        {this.renderErrors()}
+      </div>
+    );
+  }
+}
+
+export default withRouter(SessionForm);
