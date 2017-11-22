e18eab78876387c43ff50d9f71262233967ac0f7
Qiumin Yin <mqyin@g.ucla.edu>
2017-11-21 18:41:15 -0800

    make auth actions work

--- frontend/messenger_clone.jsx
+++ frontend/messenger_clone.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
-import { login, logout, signup } from './util/session_api_util';
+import { login, logout, signup } from './actions/session_actions';
 
 
 document.addEventListener('DOMContentLoaded', () => {
