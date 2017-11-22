b0b9e83653b183241ae405b820bfb004cd8be7da
Qiumin Yin <mqyin@g.ucla.edu>
2017-11-21 17:04:11 -0800

    add backend auth

---
+++ frontend/components/root.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import { Provider } from 'react-redux';
+// import { HashRouter } from 'react-router-dom';
+//import App from './App';
+
+// const Root = ({ store }) => (
+//   <Provider store={ store }>
+//     <HashRouter>
+//       <h1>React is working</h1>
+//     </HashRouter>
+//   </Provider>
+// );
+
+const Root = () => (
+  <h1>React is working</h1>
+);
+
+
+export default Root;
---
+++ frontend/messenger_clone.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/root';
+//import configureStore from './store/store';
+//import { login, logout, signup } from './actions/session_actions';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+
+  // const store = configureStore();
+  // window.store = store;
+
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  // window.login = login;
+  // window.signup = signup;
+  // window.logout = logout;
+  ReactDOM.render(<h1>Welcome to Messenger</h1>,root);
+  //ReactDOM.render(<Root store={store}/>, root);
+});
