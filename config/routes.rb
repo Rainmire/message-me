a55813ce760ae85d5bba6bfdadceb826370638a3
Qiumin Yin <mqyin@g.ucla.edu>
2017-11-23 15:44:49 -0800

    update routes for messages, conversations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
-    resource :users, only: [:create, :show]
+    resources :users, only: [:create, :show]
     resource :session, only: [:create, :destroy, :show]
+    resources :conversations, only: [:create, :destroy, :show, :update]
+    resources :messages, only: [:create]
   end
   root "static_pages#root"
 end
