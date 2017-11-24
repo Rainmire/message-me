22ecf172f082f0fa56d81ef6bc853defe892bd39
Qiumin Yin <mqyin@g.ucla.edu>
2017-11-23 16:02:01 -0800

    start revising routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :show]
     resource :session, only: [:create, :destroy, :show]
-    resources :conversations, only: [:create, :destroy, :show, :update]
+
+
+
+    resources :conversations, only: [:create, :destroy, :show, :update] do
+      get 'messages', on: :collection
+    end
+
+
     resources :messages, only: [:create]
   end
   root "static_pages#root"
