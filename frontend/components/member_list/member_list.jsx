c31c3b76863fe6f346e5fefe16dccba259f04951
Qiumin Yin <mqyin@g.ucla.edu>
2017-11-30 10:56:19 -0800

    style member list, fix chat input sizing

--- frontend/components/member_list/member_list.jsx
+++ frontend/components/member_list/member_list.jsx
@@ -6,13 +6,20 @@ class ConversationList extends React.Component {
     const { members } = this.props;
 
     return(
-      <div className="members-box">
-        <h1>People</h1>
-        <ul className="members-list">
-          <li className="add-member">Add Member</li>
-            {members.map((member,idx) => (
-              <li key={idx} className="member-list-item">{member.display_name}</li>
-            ))}
+      <div className="member-box">
+        <ul className="member-list">
+          <li className="member-list-title">Members</li>
+          <li className="add-member">
+            <div className="add-member-plus">
+              &#43;
+            </div>
+            <div className="add-member-text">
+              Add Member
+            </div>
+          </li>
+          {members.map((member,idx) => (
+            <li key={idx} className="member-list-item">{member.display_name}</li>
+          ))}
         </ul>
       </div>
     );
