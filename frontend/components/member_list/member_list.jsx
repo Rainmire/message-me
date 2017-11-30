00ad1f533c51be26104a3e77f5d56232c77f07eb
Qiumin Yin <mqyin@g.ucla.edu>
2017-11-30 13:27:59 -0800

    add member button renders, about to start search

--- frontend/components/member_list/member_list.jsx
+++ frontend/components/member_list/member_list.jsx
@@ -2,6 +2,55 @@ import React from 'react';
 
 class ConversationList extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clickAddMember: false,
+      memberId: ''
+    };
+    this.addMemberButton = this.addMemberButton.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  addMemberButton() {
+    if (this.state.clickAddMember) {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <input type="text"
+            onChange={this.update('memberId')}
+            placeholder= "Enter user id..."
+          />
+        </form>
+      );
+    }
+    else {
+      return (
+        <button className="add-member-button" onClick={()=>this.setState({clickAddMember: true})} >
+          <div className="add-member-plus">
+            &#43;
+          </div>
+          <div className="add-member-text">
+            Add Member
+          </div>
+        </button>
+      );
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    // const memberId = this.state.memberId;
+    // this.props.processForm(memberId);
+    this.setState({clickAddMember: false});
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
   render() {
     const { members } = this.props;
 
@@ -10,12 +59,7 @@ class ConversationList extends React.Component {
         <ul className="member-list">
           <li className="member-list-title">Members</li>
           <li className="add-member">
-            <div className="add-member-plus">
-              &#43;
-            </div>
-            <div className="add-member-text">
-              Add Member
-            </div>
+            {this.addMemberButton()}
           </li>
           {members.map((member,idx) => (
             <li key={idx} className="member-list-item">{member.display_name}</li>
