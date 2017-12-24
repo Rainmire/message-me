0c6b3ed2605a44f1b048a518070ba5500e8a39a5
Qiumin Yin <mqyin@g.ucla.edu>
2017-12-24 12:44:56 -0800

    make gear toggle

--- frontend/components/navbar/navbar_banner/navbar_banner.jsx
+++ frontend/components/navbar/navbar_banner/navbar_banner.jsx
@@ -2,14 +2,42 @@ import React from 'react';
 import UserSettings from './user_settings';
 import CreateConversationButton from './create_conversation_button';
 
-const NavbarBanner = () => (
-  <div className="navbar-banner">
-    <i className="fa fa-cog" aria-hidden="true">
-      <UserSettings />
-    </i>
-    <CreateConversationButton />
-  </div>
-);
+class NavbarBanner extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      clickGear: false
+    };
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+  }
+
+  toggleDropdown () {
+    let clickState = this.state.clickGear ? false : true;
+    this.setState({
+      clickGear: clickState
+    });
+  }
+
+  dropdown() {
+    if (this.state.clickGear) {
+      return <UserSettings/>;
+    }
+  }
+
+  render () {
+
+    return (
+      <div className="navbar-banner">
+        <i className="fa fa-cog" aria-hidden="true">
+          <button onClick={this.toggleDropdown}/>
+        </i>
+        {this.dropdown()}
+        <CreateConversationButton />
+      </div>
+    );
+  }
+}
 
 export default NavbarBanner;
 
