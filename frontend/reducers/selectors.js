811e00569bf0e80b6bac7db97d4ebce9ab50025d
Qiumin Yin <mqyin@g.ucla.edu>
2017-12-25 01:02:50 -0800

    rendering all conversation list info

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -2,14 +2,11 @@ import values from 'lodash/values';
 
 export const selectAllMessages = state => values(state.entities.messages);
 
-// export const selectAllConversations = state => values(state.entities.conversations);
-
 export const selectAllConversations = state => {
   const conversations = state.entities.conversations;
-  return Object.keys(conversations).map(id=>({id,title:conversations[id].title}));
+  return Object.keys(conversations).map(id=>(conversations[id]));
 };
 
 export const selectAllMembers = state => values(state.entities.members);
 
 export const selectAllUserSearchResults = state => values(state.entities.userSearchResults);
-// export const selectAllUserSelections = state => values(state.entities.userSelections);
