61c877b46a82ebae056227e458912614c79da8be
Qiumin Yin <mqyin@g.ucla.edu>
2018-01-03 11:23:29 -0800

    fix create user

--- frontend/components/new_conversation/new_conversation_container.js
+++ frontend/components/new_conversation/new_conversation_container.js
@@ -3,12 +3,16 @@ import { createConversation } from 'actions/conversation_actions';
 import { fetchConversations } from 'actions/conversation_list_actions';
 import NewConversation from './new_conversation';
 
+const mapStateToProps = (state) => ({
+  conversations: state.entities.conversations
+});
+
 const mapDispatchToProps = (dispatch) => ({
   createConversation: (users) => dispatch(createConversation(users)),
   fetchConversations: () => dispatch(fetchConversations())
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(NewConversation);
