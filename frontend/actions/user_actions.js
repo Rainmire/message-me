241df8de9b134eee8d68f63821e46c9e61291ec0
Qiumin Yin <mqyin@g.ucla.edu>
2018-01-07 20:12:46 -0800

    fix clear state on logout

--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -1,11 +1,16 @@
 import * as APIUtil from '../util/user_api_util';
 export const RECEIVE_USER_SEARCH_RESULTS = 'RECEIVE_USER_SEARCH_RESULTS';
+export const CLEAR_USER_SEARCH_RESULTS = 'CLEAR_USER_SEARCH_RESULTS';
 export const RECEIVE_USER_SELECTION = 'RECEIVE_USER_SELECTION';
 export const CLEAR_USER_SELECTIONS = 'CLEAR_USER_SELECTIONS';
 
-const receiveUserSearchResults = (users) => ({
+export const receiveUserSearchResults = (users) => ({
   type: RECEIVE_USER_SEARCH_RESULTS,
   users
+});
+
+export const clearUserSearchResults = () => ({
+  type: CLEAR_USER_SEARCH_RESULTS
 });
 
 export const receiveUserSelection = (user) => ({
