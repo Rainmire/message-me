a66fc122dcbd65e9b49f957c2ed78bc9227f321f
Qiumin Yin <mqyin@g.ucla.edu>
2018-01-08 14:49:17 -0800

    pass correct props

--- frontend/components/conversation_specific/message_list/message_list_item.jsx
+++ frontend/components/conversation_specific/message_list/message_list_item.jsx
@@ -4,31 +4,29 @@ import {toLocalTime} from 'util/local_time_conversion';
 class MessageListItem extends React.Component {
 
   render() {
-    const messageListItem = (message, idx) => {
-      const { members, currentUserId } = this.props;
-      const author = members[message.user_id];
-      const messageClass = author.id===currentUserId ? "message-item my-message" : "message-item their-message";
-      const timestamp = toLocalTime(message.created_at);
-      return (
-        <li key={idx} className={messageClass}>
-          <img className="author-pic" src={author.profile_pic} />
-          <div className="message-text">
-            <div className="message-info">
-              <div className="author-name">
-                {author.display_name}
-              </div>
-              <div className="timestamp">
-                {timestamp}
-              </div>
+    const { message, idx, author, currentUserId } = this.props;
+    const messageClass = author.id===currentUserId ? "message-item my-message" : "message-item their-message";
+    const timestamp = toLocalTime(message.created_at);
+    return (
+      <li key={idx} className={messageClass}>
+        <img className="author-pic" src={author.profile_pic} />
+        <div className="message-text">
+          <div className="message-info">
+            <div className="author-name">
+              {author.display_name}
             </div>
-            <div className="message-body">
-              {message.body}
+            <div className="timestamp">
+              {timestamp}
             </div>
           </div>
-        </li>
-      );
-    };
+          <div className="message-body">
+            {message.body}
+          </div>
+        </div>
+      </li>
+    );
   }
+
 }
 
 
