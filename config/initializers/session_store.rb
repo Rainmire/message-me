b7631b341657cff809087a04dca62038ff8bded4
Qiumin Yin <mqyin@g.ucla.edu>
2018-01-18 14:10:27 -0800

    remove session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-Rails.application.config.session_store :cookie_store, {
-  key: '_your_app_session',
-  domain: '.message-me.live'
-}
+# Rails.application.config.session_store :cookie_store, {
+#   key: '_your_app_session',
+#   domain: '.message-me.live'
+# }
