0e9ff3963cca247fc052d717215a1b4625de66c5
Qiumin Yin <mqyin@g.ucla.edu>
2018-01-18 14:03:13 -0800

    no production change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
-  config.action_controller.session = { :domain => '.message-me.live' }
+  # config.action_controller.session = { :domain => '.message-me.live' }
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
